b2cf0bd3e00d34bc715c812a6e76dfb08845856b
Diogo Sales <diogosalesdev@gmail.com>
2022-05-06 14:20:30 -0300

    fix: errors adjustments

--- src/routes.ts
+++ src/routes.ts
@@ -6,22 +6,33 @@ import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 export const routes = express.Router();
 
 routes.post('/feedbacks', async (req, res) => {
-	const { type, comment, screenshot } = req.body;
+	try {
+		const { type, comment, screenshot } = req.body;
 
-	const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-	const nodemailerMailAdapter = new NodemailerMailAdapter();
+		const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+		const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-	const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+		const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
 
-	await submitFeedbackUseCase.execute({
-		type,
-		comment,
-		screenshot
-	});
+		await submitFeedbackUseCase.execute({
+			type,
+			comment,
+			screenshot
+		});
 
-	return res.status(201).send();
+		return res.status(201).send();
+	} catch (error) {
+		console.log(error);
+
+		return res.status(500).send();
+	}
 });
 
 routes.get('/healthcheck', (req, res) => {
-	return res.status(200).send();
+	try {
+		return res.status(200).send();
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send();
+	}
 });
